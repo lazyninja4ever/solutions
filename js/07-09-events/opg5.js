673f2fa9f57e28f7e384720f432ce32d585a78e7
unknown <mette.tanggaard.ohlsen@gmail.com>
2022-09-08 15:58:05 +0200

    opg 5 update

--- js/07-09-events/opg5.js
+++ js/07-09-events/opg5.js
@@ -2,25 +2,11 @@ let btn = document.getElementById('partyBtn');
 let background = document.querySelector('body');
 let isOn = false; //holder styr på om "party" er on eller off
 
+let tjeckParty = () => {
+  isOn ? btn.innerHTML = 'Party off' :  btn.innerHTML = 'Party on'; //tjek om party er on eller off, ret teksten
+  background.classList.toggle('neutral'); //hvis den er der bliver den fjerent, hvis den ikke er der bliver den tilføjet
+  background.classList.toggle('anim');
+  isOn = !isOn; //ændre status på party on til off, eller off til on
+}
+
 btn.addEventListener('click', tjeckParty);
-
-
-
-function tjeckParty(){
-  isOn ? partyOff() : partyOn();
-}
-
-let partyOn = () => {
-  btn.innerHTML = 'Party off';
-  background.classList.remove('neutral');
-  background.classList.add('anim');
-  isOn = true;
-
-}
-
-let partyOff = () => {
-  btn.innerHTML = 'Party on';
-  background.classList.add('neutral');
-  background.classList.remove('anim');
-  isOn = false;
-}
